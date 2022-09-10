7a433fdc3c4983946dfd2286f053f3b3d82679ef
Aleks_Tsud <100237478+Alekstsud@users.noreply.github.com>
2022-09-10 19:38:41 +0200

    refactor: add switch, break, include

--- php_constructs.php
+++ php_constructs.php
@@ -154,7 +154,242 @@ echo $name;
 }
 ?>
 
+foreach (array as $value) {
+  код для выполнения;
+}
+//или
+foreach (array as $key => $value) {
+   код для выполнения;
+}
+
+<?php
+$names = array("John", "David", "Amy");
+foreach ($names as $name) {
+   echo $name.'<br />';
+}
+
+// Выведет:
+// John
+// David
+// Amy
+?>
+
+<?php
+$items = array("one", "two", "three");
+foreach ($items as $item) {
+echo "$item\n";
+}
+?>
+
+<?php
+switch (n) {
+  case value1:
+    //код будет выполнен, если n=value1
+    break;
+  case value2:
+     //код будет выполнен, если n=value2
+     break;
+  ...
+  default:
+    // код будет выполнен, если n отличается от всех перечисленных значений
+}
+?>
+
+<?php
+$today = 'Tue';
+
+switch ($today) {
+  case "Mon":
+    echo "Today is Monday.";
+    break;
+  case "Tue":
+    echo "Today is Tuesday.";
+    break;
+  case "Wed":
+    echo "Today is Wednesday.";
+    break;
+  case "Thu":
+    echo "Today is Thursday.";
+    break;
+  case "Fri":
+     echo "Today is Friday.";
+     break;
+  case "Sat":
+     echo "Today is Saturday.";
+     break;
+  case "Sun":
+     echo "Today is Sunday.";
+     break;
+  default:
+     echo "Invalid day.";
+}
+//Выведет "Today is Tuesday."
+?>
+
+<?php
+
+$x = 5;
+switch ($x) {
+  case 1:
+    echo "One";
+    break;
+  case 2:
+    echo "Two";
+    break;
+  default:
+    echo "No match";
+}
+
+//Выведет "No match"
+?>
+
+<?php
+$day = 'Wed';
+
+switch ($day) {
+case 'Mon':
+    echo 'First day of the week';
+    break;
+case 'Tue':
+case 'Wed':
+case 'Thu':
+    echo 'Working day';
+    break;
+case 'Fri':
+    echo 'Friday!';
+    break;
+default:
+    echo 'Weekend!';
+}
+
+//Выведет "Working day"
+?>
+
+<?php
+$x=1;
+switch ($x) {
+  case 1:
+    echo "One";
+  case 2:
+    echo "Two";
+  case 3:
+    echo "Three";
+  default:
+    echo "No match";
+}
+
+//Выведет "OneTwoThreeNo match"
+
+// Break также может быть использован, чтобы прервать выполнение циклов for, foreach, while, do-while.
+?>
+
+<?php
+for ($i = 0; $i <= 50; $i++) {
+    echo $i;
+    if ($i == 5) {
+      break;
+    }
+  }
+?>
+
+<?php
+// пример пропускает четные числа в цикле for:
+
+for ($i = 0; $i < 10; $i++) {
+  if ($i%2 == 0) {
+    continue;
+  }
+  echo $i . ' ';
+}
+
+// Выведет: 1 3 5 7 9
+?>
+
+<?php
+for ($i = 0; $i <= 15; $i++) {
+  if ($i == 10 || $i == 14) {
+    continue;
+  }
+  echo $i."\n"; // код вывел все числа от 0 до 15 (включительно), за исключением чисел 10 и 14.
+}
+?>
 
 
+
+Операторы include и require позволяют вставить содержимое одного PHP-файла в другой PHP-файл до того, как сервер его выполнит
+
+<?php
+  echo '<h1>Welcome</h1>';
+?>
+
+<html>
+  <body>
+
+  <?php include 'header.php'; ?>
+
+  <p>Some text.</p>
+  <p>Some text.</p>
+  </body>
+</html>
+
+Используйте require, когда файл необходим для запуска приложения.
+Используйте include, когда файл не является обязательным. Приложение должно продолжать работу, даже если файл не найден.
+
+<?php
+for ($i = 0; $i <= 10; $i++) {
+    if ($i%2 == 0)
+    echo $i."\n"; //Используя цикл for, выведите только четные числа от 0 до 10 (включительно).
+}
+?>
+
+
+<?php
+$x = 6;
+if ($x == 10) {
+      echo "A";
+}
+elseif ($x > 7 && $x < 10) {
+       echo "B";
+}
+elseif ($x == 20) {
+       echo "C";
+}
+else {
+       echo "D"; //D
+}
+?>
+
+<?php
+$x = readline();
+$y = readline();
+if ($x > $y) {
+      echo $x;
+}
+else {
+       echo $y; 
+}
+?>
+
+
+<?php
+    $a = readline();
+    $b = readline();
+for ($i = $a; $i <= $b; $i++) {
+  if ($i%2 == 0) 
+  echo $i . ' '; //Выведите (через пробел) все четные числа от a до b (включительно).
+}
+?>
+
+
+
+<?php
+    $a = readline();
+    $x;
+for ($i = 0; $i <= $a; $i++) { 
+    $y=readline();
+    $x += $y; } 
+echo $x; 
+//Вводится число N, а затем N чисел, сумму которых необходимо вычислить..
+?>
 
 
